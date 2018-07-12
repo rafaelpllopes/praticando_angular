ce72db7b37963fa1482e43681f40a8d1c5ba349f
rafaelpllopes <rafelpllopes@gmail.com>
2018-07-12 20:31:47 -0300

    Criado componente photos para melhorar a exibição no photos-list.

--- picture/src/app/photos/photos.module.ts
+++ picture/src/app/photos/photos.module.ts
@@ -5,12 +5,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { PhotoComponent } from './photo/photo.component';
 import { PhotoListComponent } from './photo-list/photo-list.component';
 import { PhotoFormComponent } from './photo-form/photo-form.component';
+import { PhotosComponent } from './photo-list/photos/photos.component';
 
 @NgModule({
   imports: [
     CommonModule,
     HttpClientModule
   ],
-  declarations: [PhotoComponent, PhotoListComponent, PhotoFormComponent],
+  declarations: [PhotoComponent, PhotoListComponent, PhotoFormComponent, PhotosComponent],
 })
 export class PhotosModule { }
