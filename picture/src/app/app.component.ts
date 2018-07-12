e061cb78b20db62dd3f5d49fa33c587d1888163f
rafaelpllopes <rafael.pllopes@gmail.com>
2018-07-12 09:13:51 -0300

    Adicionado bootstrap 4 para usar no projeto.

--- picture/src/app/app.component.ts
+++ picture/src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Picture';
+
+  photos = [
+    {
+      url: "http://www.diarioonline.com.br/app/painel/modulo-noticia/img/imagensdb/original/destaque-436195-digimon.full_.1707809-1024x562.jpg",
+      description: "Dimons Lideres"
+    },
+    {
+      url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqQrL386wJv6dS1ZUiBu3CbtBYg6bB2ejHzdq7nzntve1tIQvqeQ",
+      description: "Dimons Luta"
+    },
+    {
+      url: "http://www.diarioonline.com.br/app/painel/modulo-noticia/img/imagensdb/original/destaque-436195-digimon.full_.1707809-1024x562.jpg",
+      description: "Dimons Lideres"
+    },
+    {
+      url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqQrL386wJv6dS1ZUiBu3CbtBYg6bB2ejHzdq7nzntve1tIQvqeQ",
+      description: "Dimons Luta"
+    }
+  ];
 }
---
+++ picture/src/app/photos/photo/photo.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-photo',
+  templateUrl: './photo.component.html',
+  styleUrls: ['./photo.component.css']
+})
+export class PhotoComponent implements OnInit {
+
+  @Input() url = '';
+  @Input() description = '';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
