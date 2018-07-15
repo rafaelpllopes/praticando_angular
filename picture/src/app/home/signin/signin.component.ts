5f6148e7fa651d937c04688cb578f7ff201c94ad
rafaelpllopes <rafelpllopes@gmail.com>
2018-07-14 21:27:55 -0300

    Criado serviço para login, continuando a implementar a autenticação.

--- picture/src/app/home/signin/signin.component.ts
+++ picture/src/app/home/signin/signin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from '../../core/auth.service';
 
 @Component({
   templateUrl: './signin.component.html',
@@ -9,13 +10,27 @@ export class SigninComponent implements OnInit {
 
   loginForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, private authService: AuthService) { }
 
   ngOnInit() {
     this.loginForm = this.formBuilder.group({
       userName: ['', Validators.required],
       password: ['', Validators.required]
     });
+  }
+
+  login() {
+    const userName = this.loginForm.get('userName').value;
+    const password = this.loginForm.get('password').value;
+
+    this.authService
+      .authenticate(userName, password)
+      .subscribe(() => console.log('autenticado'), err => {
+        console.log(err);
+        this.loginForm.reset();
+      });
+
+
   }
 
 }
