f67f184b7099fdbe1e38e6e5f60f36edeb79398f
rafaelpllopes <rafelpllopes@gmail.com>
2018-08-15 20:49:33 -0300

    Tornando o titulo da pagina dinamico de acordo com a rota ativa.

--- picture/src/app/home/home.routing.module.ts
+++ picture/src/app/home/home.routing.module.ts
@@ -13,11 +13,17 @@ const routes: Routes = [
         children: [
             { 
                 path: '', 
-                component: SigninComponent 
+                component: SigninComponent ,
+                data: {
+                    title: 'Sign in'
+                }
             },
             { 
                 path: 'signup', 
-                component: SignupComponent 
+                component: SignupComponent ,
+                data: {
+                    title: 'Sign up'
+                }
             },        
         ]
     }
