6046b75f23ca0715d616d1910e07f4c8047fdbdc
rafaelpllopes <rafael.pllopes@gmail.com>
2018-09-13 20:24:28 -0300

    Modificando a validação em sigup component

--- picture/src/app/home/signup/signup.component.ts
+++ picture/src/app/home/signup/signup.component.ts
@@ -6,11 +6,12 @@ import { NewUser } from './new-user';
 import { SignupService } from './signup.service';
 import { Router } from '@angular/router';
 import { PlatformDetectorService } from '../../core/platform-detector/platform-detector.service';
+import { userNamePassword } from './usernaem-password.validator';
 
 @Component({
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css'],
-  providers: [ UserNotTakenValidatorService ]
+  providers: [UserNotTakenValidatorService]
 })
 export class SignupComponent implements OnInit {
 
@@ -23,33 +24,37 @@ export class SignupComponent implements OnInit {
     this.signupForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       fullName: ['', [
-        Validators.required, 
-        Validators.minLength(2), 
+        Validators.required,
+        Validators.minLength(2),
         Validators.maxLength(40)
       ]
-    ],
+      ],
       userName: ['', [
-        Validators.required, 
-        Validators.minLength(2), 
+        Validators.required,
+        Validators.minLength(2),
         Validators.maxLength(30),
         lowerCaseValidator
         //Validators.pattern(/^[a-z0-9_\-]+$/)
       ],
-      this.userNotTakenValidatorService.checkUserNameTaken()
-    ],
+        this.userNotTakenValidatorService.checkUserNameTaken()
+      ],
       password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(14)]]
+    },{
+      validator: userNamePassword
     });
 
     this.paltformDetectorService.isPlatformBrowser() && this.inputEmail.nativeElement.focus();
   }
 
   signup() {
-    const newUser = this.signupForm.getRawValue() as NewUser;
-    this.signupService
-      .signup(newUser)
-      .subscribe(()=> this.router.navigate(['']), 
-        err => console.log(err)
-      );
+    if (this.signupForm.valid && !this.signupForm.pending) {
+      const newUser = this.signupForm.getRawValue() as NewUser;
+      this.signupService
+        .signup(newUser)
+        .subscribe(() => this.router.navigate(['']),
+          err => console.log(err)
+        );
+    }
   }
 
 }
